15b2a9afd318d13211a584b0f53e27ebb107e384
Isaac Wagner <isaacbwagner@gmail.com>
2013-04-23 15:27:13 -0400

    Added and commented out function stubs for setting/retrieving pairs with []

--- src/hashtable.h
+++ src/hashtable.h
@@ -16,8 +16,15 @@ class HashTable : public node::ObjectWrap {
   typedef std::unordered_map<std::string,v8::Persistent<v8::Value>> MapType;
   MapType map;
 
+
   //new HashTable() or new HashTable(buckets)
   static v8::Handle<v8::Value> Constructor(const v8::Arguments &args);
+
+  //hashTable[key] : value
+  static v8::Handle<v8::Value> GetNamedProperty(v8::Local<v8::String> key, const v8::AccessorInfo &info);
+
+  //hashTable[key] = value
+  static v8::Handle<v8::Value> SetNamedProperty(v8::Local<v8::String> key, v8::Local<v8::Value> value, const v8::AccessorInfo &info);
 
   //hashTable.get(key) : value
   static v8::Handle<v8::Value> Get(const v8::Arguments &args);
