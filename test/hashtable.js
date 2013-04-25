9cd0e3cb0ea17ded5fa93c4471a9cad73b0b2f30
Isaac Wagner <isaacbwagner@gmail.com>
2013-04-24 22:17:47 -0400

    Changed tests to 'dot-after' formatting

--- test/hashtable.js
+++ test/hashtable.js
@@ -3,19 +3,27 @@ var HashMap = require('..'),
 
 var hashmap = new HashMap();
 
-test.ok(hashmap, 'new HashMap()')
-    .is(function() {
+test.
+
+    ok(hashmap, 'new HashMap()').
+
+    is(function() {
         hashmap.max_load_factor(0.75);
         return hashmap.max_load_factor();
-    }, 0.75, 'get/set max_load_factor')
-    .test("object identity", function(test) {
+    }, 0.75, 'get/set max_load_factor').
+
+    test("object identity", function(test) {
         var obj = { hello: 'world' };
         hashmap.put('obj', obj);
 
-        test.is(hashmap.get('obj'), obj)
-            .isnt(hashmap.get('obj'), { hello: 'world' })
-            .done();
-    })
-   .done();
+        test.
+            is(hashmap.get('obj'), obj).
+
+            isnt(hashmap.get('obj'), { hello: 'world' }).
+
+            done();
+    }).
+    
+    done();
 
 
