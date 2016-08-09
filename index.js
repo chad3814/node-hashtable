464093622c7499f0bd3973d391cbbeebd284338f
Chad Walker <chad@chad-cat-lore-eddie.com>
2016-08-09 14:50:11 -0700

    added a small toJSOn() on the prototype in javascript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./build/Release/native').HashTable;
+var HashTable = require('./build/Release/native').HashTable;
+HashTable.prototype.toJSON = function() {
+    var ret = {};
+    this.forEach(function(key, value) {
+        ret[String(key)] = value;
+    });
+
+    return ret;
+};
+
+module.exports = HashTable;
